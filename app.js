679c0f7f884fcadcfa0bb0aa90ece3285f375cc0
Rossi Oddet <rossi.oddet@gmail.com>
2015-08-05 07:06:08 +0200

    Initialisation Projet ECommerce Desktop

---
+++ app.js
@@ -0,0 +1,24 @@
+angular.module('ecDesktopApp', [
+    'ui.utils',
+    'ngRoute',
+    'ngAnimate',
+    'ecDesktopApp.shared',
+    'ecDesktopApp.home'
+]);
+
+angular.module('ecDesktopApp').config(function($routeProvider) {
+
+    // Ici, les routes générales de l'application
+    // Pas de route spécifique ici !
+    // Elles doivent être déclarées dans des sous-modules (comme 'home')
+    $routeProvider.otherwise({redirectTo:'/home'});
+});
+
+angular.module('ecDesktopApp').run(function($rootScope) {
+
+});
+
+// Contrôleur qui pilote globalement l'application
+angular.module('ecDesktopApp').controller("ecDesktopCtrl", function() {
+    this.title = "ECommerce Desktop";
+});
