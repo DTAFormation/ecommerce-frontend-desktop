7cd5db060e9bdc1b70457e9ecc844ee67f8405d0
nstoffels <n.stoffels@groupe-esigelec.fr>
2015-08-05 16:48:33 +0200

    développement d'afficher produit en cours, modification de l'index.html finit

---
+++ product/listproduct.js
@@ -0,0 +1,20 @@
+// Déclaration du module 'listproduct'
+angular.module('ecDesktopApp.listproduct', [
+    'ngRoute',
+]);
+
+// Configuration du module 'listproduct'
+angular.module('ecDesktopApp.listproduct').config(function($routeProvider) {
+
+    // TODO Définir les routes spécifiques au module 'listproduct' ici
+});
+
+// Contrôleur principal du module 'listproduct'
+// Usage de la syntaxe 'controller as', pas besoin du '$scope'
+angular.module('ecDesktopApp.listproduct').controller('homeCtrl', function(userService) {
+
+    var self = this;
+
+    // ...
+
+});
