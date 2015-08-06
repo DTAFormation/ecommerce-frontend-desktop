cdf1d05466314dc10dd1e23b5a736c9ba6a098b9
nstoffels <n.stoffels@groupe-esigelec.fr>
2015-08-06 10:03:38 +0200

    MaJ de la présentation des produits, mis en place d'une table propre

--- product/service/listproduct.service.js
+++ product/service/listproduct.service.js
@@ -3,7 +3,8 @@ angular.module('ecDesktopApp.product').service('productService', function($http)
 // TODO Service Home
 this.getProducts = function(){
      var bouchonproduct="data/bouchonproduct.json";
-     return $http.get(bouchonproduct);
+     console.log($http.get(bouchonproduct)); 
+     return $http.get(bouchonproduct);  
     };
 
 });
