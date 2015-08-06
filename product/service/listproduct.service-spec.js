304a146b15cc9c84c4b10194c17cb996c351009b
nstoffels <n.stoffels@groupe-esigelec.fr>
2015-08-06 16:41:55 +0200

    test unitaire sur le service listproduct.service fait.

--- product/service/listproduct.service-spec.js
+++ product/service/listproduct.service-spec.js
@@ -2,15 +2,35 @@
 
 describe('listproductServiceTest', function(){
     //test
-    it("FonctionListProductTest", function(){
-        //code du test de vérification
-        this.listProducts=function(){  
-            return [  
-            {id : 1, libelle : "libelle" , caracteristique : "carac", categorie : "categorie", image : "image" , prix : 0.10},  
-            {id : 2, libelle : "libelle2" , caracteristique : "caracteristique", categorie : "categorie2", image : "image2" , prix : 0.20}  
-            ];  
-        }; 
-
-        
+    beforeEach(function(){
+        module("ecDesktopApp.product");
     });
+
+    it("est ce que la fonction cherche la liste du produit? ", inject(function(productService,$httpBackend){
+        //code du test de vérification
+
+        //simulation de la réponse que l'on recevra
+        var reponseSimule={id : 1, libelle : "libelle" , caracteristique : "carac", categorie : "categorie", image : "image" , prix : 0.10};
+
+        //l'adresse sur laquelle on récupère les données
+        $httpBackend.when("GET", "data/bouchonproduct.json").respond(reponseSimule);
+
+        //récupération des données
+        var reponsePromesse=productService.getProducts();
+
+        // vérification du résultat
+        reponsePromesse.then(function(response){
+            var product = response.data;
+            expect(product.id).toEqual(1);
+            expect(product.libelle).toEqual("libelle");
+            expect(product.caracteristique).toEqual("carac");
+            expect(product.categorie).toEqual("categorie");
+            expect(product.image).toEqual("image");
+            expect(product.prix).toEqual(0.10);
+        });
+        
+        //pour déclencher les réponses des requêtes faites avec $http.
+        $httpBackend.flush();
+
+    }));
 });
