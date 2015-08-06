420660bbebe384e6a5205f9c2d23a2860666f235
nstoffels <n.stoffels@groupe-esigelec.fr>
2015-08-06 14:29:38 +0200

    préparation du test unitaire

--- product/service/listproduct.service.js
+++ product/service/listproduct.service.js
@@ -1,6 +1,6 @@
 angular.module('ecDesktopApp.product').service('productService', function($http) {
 
-// TODO Service Home
+// Fonction qui récupère les données dans le json. les affiches dans la console comme promise.
 this.getProducts = function(){
      var bouchonproduct="data/bouchonproduct.json";
      console.log($http.get(bouchonproduct)); 
