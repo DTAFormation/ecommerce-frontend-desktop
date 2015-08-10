5e3c6545e71c44e5ac7f14666a66c69490690820
Jenkins <jenkins@ns377570.ip-5-196-89.eu>
2015-08-10 17:41:00 +0200

    auto deploy

--- deploy/product/service/product.service.js
+++ deploy/product/service/product.service.js
@@ -20,6 +20,7 @@ angular.module('ecDesktopApp.product').service('productService', function($http)
     //supprimer un produit en base
     this.deleteProduct = function(product){
         console.log("tentative de suppression d'un produit"+product);
+        console.log(delUrl+product.id+"/bouchonproduct.json");
         return $http.delete(delUrl+product.id+"/bouchonproduct.json");
     };
 
