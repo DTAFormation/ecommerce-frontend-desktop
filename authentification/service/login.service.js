20224443c241c82be4ce55dcec51255dcb91be2e
nstoffels <n.stoffels@groupe-esigelec.fr>
2015-08-11 14:06:00 +0200

    fonctionnement test avec la table users fonctionne pour #1, et localhost/admin fonctionne aussi, test en intégration

--- authentification/service/login.service.js
+++ authentification/service/login.service.js
@@ -4,8 +4,8 @@ angular.module('ecDesktopApp.authentification')
         var service = {};
         //var apiUrl = "http://localhost:8085/ecommerce-backend/api/user/connect";
         //var apiUrl = "http://5.196.89.85:9000/ec-backend/api/user/connect";
-        var apiUrl = "http://localhost:8085/ecommerce-backend/api/admin/connect/";
-        //var apiUrl = "http://5.196.89.85:9000/ec-backend/api/admin/connect";
+        //var apiUrl = "http://localhost:8085/ecommerce-backend/api/admin/connect/";
+        var apiUrl = "http://5.196.89.85:9000/ec-backend/api/admin/connect";
 
         service.Login = function (username, password, callback) {
             var userData = { "login" : username, "password" : password };
