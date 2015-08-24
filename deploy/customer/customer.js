5f19a60dab420bd11977a0a62eb44ebae04035eb
Jenkins <jenkins@ns377570.ip-5-196-89.eu>
2015-08-24 16:51:15 +0200

    auto deploy

--- deploy/customer/customer.js
+++ deploy/customer/customer.js
@@ -36,18 +36,28 @@ angular.module('ecDesktopApp.customer').config(function($routeProvider) {
 
 // Contrôleur principal du module 'createCustomer'
 // Usage de la syntaxe 'controller as', pas besoin du '$scope'
-angular.module('ecDesktopApp.customer').controller('CreateCustomerController', function(customerService, $location) {
+angular.module('ecDesktopApp.customer').controller('CreateCustomerController', function(customerService, $location, $q) {
 
     var self = this;
     self.ajoutClient = function(customer){
         customerService.addCustomer(customer)
         .then(function(response){ //en cas de succes
             self.err = false;
-            console.log("client créé");
+            //console.log("client créé");
             $location.path("/customer/listcustomer");
         },function(error){
-            console.log("erreur de requete");
             self.err = true;
+        });
+    };
+
+    self.doesNotExist = function(value){
+        return $q(function(resolve, reject) {
+          customerService.getlogin(value)
+            .then(function(response){
+                reject(false);
+            },function(response){
+                resolve(true);
+            });
         });
     };
 });
@@ -56,28 +66,31 @@ angular.module('ecDesktopApp.customer').controller('CreateCustomerController', f
 angular.module('ecDesktopApp.customer').controller('customerCtrl', function (customerService) {
 
 
-    var self = this;
-    customerService.getCustomers()
-    .then(
-        function (customer) {
-                    //console.log("customer:"+JSON.stringify(customer));
-                    self.customers = customer;
-                });
+    var customerCtrl = this;
+    customerCtrl.customers = null;
 
-    self.delCustomer = function(id){
+    customerCtrl.getCustomers = function() {
+        customerService.getCustomers()
+        .then(function(customer) {
+            customerCtrl.customers = customer;
+        });
+    };
+
+    customerCtrl.delCustomer = function(id){
         console.log("function delCustomer sur le client "+id);
         customerService.deleteCustomer(id) // appel du service de suppresion d'un client
         .then(function(succes){
             console.log('succes lors de la requete de suppression de client');
-            customerService.getCustomers(); //sencer mettre a jour la liste des clients a jour
-            self.err=false;
-            //return succes.data;
-        },function(error){
-            self.err=true;
+            customerCtrl.getCustomers();
+            customerCtrl.err=false;
+        }, function(error){
+            customerCtrl.err=true;
             setTimeout(function(){window.location.reload();},2000);
             console.log('erreur lors de la requete de suppression de client');
         });
     };
+
+    customerCtrl.getCustomers();
 
 });
 
