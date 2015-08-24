45540051d175ad706f45cec981624f992d51d7d1
Jenkins <jenkins@ns377570.ip-5-196-89.eu>
2015-08-24 18:11:20 +0200

    auto deploy

--- deploy/authentification/service/login.service.js
+++ deploy/authentification/service/login.service.js
@@ -1,47 +1,21 @@
 angular.module('ecDesktopApp.authentification')
 .factory('loginService', ['$http', '$cookieStore', '$rootScope', '$timeout',
     function ($http, $cookieStore, $rootScope, $timeout) {
-        var service = {};
-        //var apiUrl = "http://localhost:8085/ecommerce-backend/api/user/connect";
-        //var apiUrl = "http://5.196.89.85:9080/ec-backend/api/user/connect";
-        //var apiUrl = "http://localhost:8085/ecommerce-backend/api/admin/connect/";
+
+        var service = this;
         var apiUrl = "http://5.196.89.85:9080/ec-backend/api/admin/connect";
 
-        service.Login = function (username, password, callback) {
-            var userData = { "login" : username, "password" : password };
-            // fonction de test pour vérifier le fonctionnement de l'authentification avec username et login valant test
-            // $timeout(function(){
-            // console.log(username + " " + password);
-            //     var response = { success: username === 'test' && password === 'test' };
-            //     if(!response.success) {
-            //         response.message = 'Username or password is incorrect';
-            //     }
-            //     callback(response);
-            // }, 1000);
-
-            console.log(username + " " + password);
-
-            //Authentification possible selon bdd, il faut cibler une url existante
-            $http.post(apiUrl, userData)
-               .then(function (response) {
-
-                response.success = response.status === 200;
-               //  var response = { success: username === 'test' && password === 'test' };
-                if(!response.success) {
-                    response.message = 'Username or password is incorrect';
-                }
-                callback(response);
-
-               });
-
+        service.Login = function (username, password) {
+          var userData = { "login" : username, "password" : password };
+          return $http.post(apiUrl, userData);
         };
- 
+
 		//setCredential permet d'initialiser les valeurs username et mdp pour la fonction test,
 		//utilisaton des services CORS (cross-origin-resources-sharing) permettant d'acceder à des services
-		//en dehors de leurs origines. 
+		//en dehors de leurs origines.
         service.SetCredentials = function (username, password) {
             var authdata = username + ':' + password;
- 
+
             $rootScope.globals = {
                 currentUser: {
                     username: username,
@@ -52,7 +26,7 @@ angular.module('ecDesktopApp.authentification')
             $http.defaults.headers.common['Authorization'] = 'Basic ' + authdata;
             $cookieStore.put('globals', $rootScope.globals);
         };
- 
+
 		//suppression des infos du cookie
         service.ClearCredentials = function () {
             $rootScope.globals = {};
@@ -60,6 +34,6 @@ angular.module('ecDesktopApp.authentification')
             //les personnes sur le site mais non connecté on par défaut une accréditation basic.
             $http.defaults.headers.common.Authorization = 'Basic ';
         };
- 
+
         return service;
     }]);
