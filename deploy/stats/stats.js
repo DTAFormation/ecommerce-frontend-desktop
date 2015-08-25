a338969cd06d5908ae217ba856aba879957484f4
Jenkins <jenkins@ns377570.ip-5-196-89.eu>
2015-08-25 14:11:39 +0200

    auto deploy

--- deploy/stats/stats.js
+++ deploy/stats/stats.js
@@ -1,6 +1,7 @@
 angular.module('ecDesktopApp.stats', [
     'ngRoute',
     'ui.bootstrap',
+	'ecDesktopApp.shared',
     'chart.js'
     ]);
 
@@ -11,8 +12,12 @@ $routeProvider
 		templateUrl:'stats/template/ventes.tpl.html',
 		controller : "ventesCtrl",
 		controllerAs:"ventesCtrl",
+	})
+	.when('/stats/bestCustomers', {
+		templateUrl : 'stats/template/bestCustomers.tpl.html',
+		controller : 'BestCustomerController',
+		controllerAs : 'bestCtrl'
 	});
-
 });
 
 
@@ -58,3 +63,57 @@ angular.module('ecDesktopApp.stats').controller('ventesCtrl', function(ventesSer
 
 });
 
+// controller pour l'écran des meilleurs clients
+angular.module('ecDesktopApp.stats').controller('BestCustomerController', function(commandeService){
+
+	var bestCtrl = this;
+
+	bestCtrl.tri = "depenses";
+	bestCtrl.customers = [];
+
+	function fetchCustomers (){
+		bestCtrl.customers = [];
+		commandeService.getCommandes().then(function (result){
+			result.forEach(function (commande){
+				var self = this;
+				self.newClient = true;
+				bestCtrl.customers.forEach(function(customer){
+					if(commande.client.id === customer.id){
+						self.newClient = false;
+						if(bestCtrl.tri === "depenses"){
+							var totalCommande = 0;
+							commande.commandeProduits.forEach(function(commandeProduit){
+								totalCommande += commandeProduit.produit.prix * commandeProduit.quantite;
+							});
+							customer.total = Math.floor(100 * (customer.total + totalCommande))/100;
+						} else {
+							customer.total += 1;
+						}
+					}
+				});
+				if(self.newClient){
+					if(bestCtrl.tri === "depenses"){
+						var totalCommande = 0;
+						commande.commandeProduits.forEach(function(commandeProduit){
+							totalCommande += commandeProduit.produit.prix * commandeProduit.quantite;
+						});
+						bestCtrl.customers.push({id:commande.client.id, nom:commande.client.nom, prenom:commande.client.prenom, total:Math.floor(100 * totalCommande)/100});
+					} else {
+						bestCtrl.customers.push({id:commande.client.id, nom:commande.client.nom, prenom:commande.client.prenom, total:1});
+					}
+				}			
+			});
+		});
+	}
+
+	fetchCustomers();
+
+	bestCtrl.changeTri = function (){
+		if(bestCtrl.tri === "depenses"){
+			bestCtrl.tri = "nbCommandes";	
+		} else {
+			bestCtrl.tri = "depenses";
+		}
+		fetchCustomers();
+	};
+});
