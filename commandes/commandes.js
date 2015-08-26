eb78a1264f7e6371231b8627c7a3a2877582f976
NathanGUILLOTEAU <guilloteau.nathan@gmail.com>
2015-08-26 11:05:08 +0200

    Changement du menu du haut, modif des tests pour le true to be false, modifs sur le design des produits, affichet commandes complet

--- commandes/commandes.js
+++ commandes/commandes.js
@@ -7,20 +7,24 @@ angular.module('ecDesktopApp.commandes', [
 angular.module('ecDesktopApp.commandes').config(function($routeProvider) {
 
     $routeProvider
-    .when('/commandes/listCommandes', {
+    .when('/commandes/listCommandes/', {
         templateUrl : "commandes/template/listCommandes.html",
         controller : "commandeCtrl",
         controllerAs : "commandeCtrl"
     });
-
 });
 
 //controlleur pour formulaire liste des commandes
-angular.module('ecDesktopApp.commandes').controller('commandeCtrl', function (commandeService) {
+angular.module('ecDesktopApp.commandes').controller('commandeCtrl', function (commandeService, ETATS_COMMANDE, ETATS_COMMANDE_LIBELLE) {
 
 
     var commandeCtrl = this;
     commandeCtrl.commandes = null;
+    commandeCtrl.etatsCommande = ETATS_COMMANDE;
+    commandeCtrl.etatsCommandeLibelle = ETATS_COMMANDE_LIBELLE;
+    commandeCtrl.etat="EC";
+    commandeCtrl.etatLibelle="En cours";
+
 
     commandeCtrl.getCommandes = function() {
         commandeService.getCommandes()
@@ -28,6 +32,27 @@ angular.module('ecDesktopApp.commandes').controller('commandeCtrl', function (co
             commandeCtrl.commandes = commandes;
         });
     };
+
+    commandeCtrl.changerEtat = function(etat) {
+      commandeService.getCommandes()
+      .then(function(commandes) {
+        commandeCtrl.etat=etat;
+        switch (etat) {
+          case "En cours":
+            commandeCtrl.etat="EC";
+            commandeCtrl.etatLibelle="En cours";
+            break;
+          case "Terminée":
+            commandeCtrl.etat="TR";
+            commandeCtrl.etatLibelle="Terminée";
+            break;
+          case "Annulée":
+            commandeCtrl.etat="AN";
+            commandeCtrl.etatLibelle="Annulée";
+            break;
+        }
+      });
+  };
 
     commandeCtrl.getCommandes();
 
