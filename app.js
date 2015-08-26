38e52596a6877814432ece78ac4027263fd654eb
Romain Farau <romain.farau@gmail.com>
2015-08-26 11:48:48 +0200

    #28 Test protractor menu fonctionnel mais non fini, constante pour les infos menus, ajout id menu

--- app.js
+++ app.js
@@ -67,28 +67,28 @@ angular.module('ecDesktopApp').controller("ecDesktopCtrl", function(loginService
     };
 });
 // Contrôleur de la navbar
-angular.module('ecDesktopApp').controller('DropdownCtrl', function ($scope) {
-    $scope.productsFunctions= [
-    {affichage:'Afficher Produits',url:'#/product/listproduct'},
-    {affichage:'Créer Produit',url:'#/product/createProduct'},
-    {affichage:'Modifier Produit',url:'#/product/listproduct'},
-    {affichage:'Supprimer Produit',url:'#/product/listproduct'}
-    ];
+angular.module('ecDesktopApp').controller('DropdownCtrl', function ($scope,DATA_MENU) {
+    $scope.productsFunctions= DATA_MENU[0];/*[
+    {affichage:'Afficher Produits',url:'#/product/listproduct',id:"afficherProduits"},
+    {affichage:'Créer Produit',url:'#/product/createProduct',id:"creerProduits"},
+    {affichage:'Modifier Produit',url:'#/product/listproduct',id:"modifierProduits"},
+    {affichage:'Supprimer Produit',url:'#/product/listproduct',id:"supprProduits"}
+    ];*/
 
-    $scope.clientsFunctions= [
-    {affichage:'Afficher Clients',url:'#/customer/listcustomer'},
-    {affichage:'Créer Client',url:'#/customer/createCustomer'}
-    ];
+    $scope.clientsFunctions= DATA_MENU[1];
+    /*{affichage:'Afficher Clients',url:'#/customer/listcustomer',id:"afficherClients"},
+    {affichage:'Créer Client',url:'#/customer/createCustomer',id:"creerClients"}
+    ];*/
 
-    $scope.ordersFunctions= [
-    {affichage:'Afficher Commandes',url:'#/commandes/listCommandes'},
-    {affichage:'Rechercher Commande par ID,Client,...',url:'#/ccccc'}
-    ];
+    $scope.ordersFunctions= DATA_MENU[2];
+    /*{affichage:'Afficher Commandes',url:'#/commandes/listCommandes',id:"afficherCommandes"},
+    {affichage:'Rechercher Commande par ID,Client,...',url:'#/ccccc',id:"rechercherCommandes"}
+    ];*/
 
-    $scope.statsFunctions= [
-    {affichage:'Afficher les meilleurs clients',url:'#/stats/bestCustomers'},
-    {affichage:'Afficher les produits les plus vendus',url:'#/bbbbb'},
-    {affichage:'Afficher les clients ayant acheté le plus un produit donné',url:'#/ccccc'},
-    {affichage:'Histogramme des ventes mensuelles cette année',url:'#/stats/Ventes'}
-    ];
+    $scope.statsFunctions= DATA_MENU[3];
+    /*{affichage:'Afficher les meilleurs clients',url:'#/stats/bestCustomers',id:"bestClients"},
+    {affichage:'Afficher les produits les plus vendus',url:'#/bbbbb',id:"bestProducts"},
+    {affichage:'Afficher les clients ayant acheté le plus un produit donné',url:'#/ccccc',id:"bestClientsOnProduct"},
+    {affichage:'Histogramme des ventes mensuelles cette année',url:'#/stats/Ventes',id:"histrogramLink"}
+    ];*/
 });
--- shared/utils/const.utils.js
+++ shared/utils/const.utils.js
@@ -1,1 +1,37 @@
 angular.module('ecDesktopApp.shared').constant("API_URL","http://5.196.89.85:9080/ec-backend/api");
+
+angular.module('ecDesktopApp.shared').constant("DATA_MENU",	[
+[{affichage:'Afficher Produits',url:'#/product/listproduct',id:"afficherProduits"},
+{affichage:'Créer Produit',url:'#/product/createProduct',id:"creerProduits"}],
+[{affichage:'Afficher Clients',url:'#/customer/listcustomer',id:"afficherClients"},
+{affichage:'Créer Client',url:'#/customer/createCustomer',id:"creerClients"}],
+[{affichage:'Afficher Commandes',url:'#/commandes/listCommandes',id:"afficherCommandes"},
+{affichage:'Rechercher Commande par ID',url:'#/commandes/rechercheCommande',id:"rechercherCommandes"}],
+[{affichage:'Afficher les meilleurs clients',url:'#/stats/bestCustomers',id:"bestClients"},
+{affichage:'Afficher les produits les plus vendus',url:'#/bbbbb',id:"bestProducts"},
+{affichage:'Afficher les clients ayant acheté le plus un produit donné',url:'#/ccccc',id:"bestClientsOnProduct"},
+{affichage:'Histogramme des ventes mensuelles cette année',url:'#/stats/Ventes',id:"histrogramLink"}]]);
+
+/*
+$scope.productsFunctions= [
+    {},
+    {}
+    ];
+
+    $scope.clientsFunctions= [
+    {},
+    {}
+    ];
+
+    $scope.ordersFunctions= [
+    {},
+    {}
+    ];
+
+    $scope.statsFunctions= [
+    {},
+    {},
+    {},
+    {}
+    ];
+*/
