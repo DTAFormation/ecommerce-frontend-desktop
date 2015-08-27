623bb2788431d7bf27d93d77a46c5bc75b846b80
Pauline KLEIN <p.klein1@outlook.fr>
2015-08-27 11:35:16 +0200

    #18 #20 lien vers bdd serveur

--- stats/service/ventes.service-spec.js
+++ stats/service/ventes.service-spec.js
@@ -1,52 +1,93 @@
 describe("Test du ventesService", function() {
 
 	var mockCommandes = [{
-      user_id: "1",
-      prix_total: 1050,
-      panier: [
-        {
-          id: 1,
-          libelle: "Produit 1",
-          prix: 150,
-          image: "http://lorempixel.com/120/120",
-          quantite: 2
-        },
-        {
-          id: 2,
-          libelle: "Produit 2",
-          prix: 150,
-          image: "http://lorempixel.com/120/120",
-          quantite: 5
-        }
-      ],
-      type_paiement: "Chèque",
-      id: 1,
-      date: "24/08/2015"
-    }];
+              "id":2,
+              "client":{
+                  "id":2,
+                  "actif":false,
+                  "nom":"GUILLOTEAU",
+                  "prenom":"Nathan",
+                  "login":"loginDeNathan",
+                  "adresses":[{"id":2,"numero":1,"rue":"rue capitaine corhumel","ville":"Nantes"},{"id":2,"numero":1,"rue":"rue capitaine corhumel","ville":"Nantes"}]
+              },
+              "commandeProduits":[{
+                "id":3,
+                "quantite":3,
+                "produit":{"id":1,"libelle":"Truc High-Tech","caracteristique":"Il sert ? rien mais il est cool","categorie":"High-Tech","image":"http://lorempixel.com/200/200/technics","prix":100.0}
+              }],
+              "etat":"Termin"
+            },
+            {
+              "id":3,
+              "client":{
+                "id":3,
+                "actif":true,
+                "nom":"LEBON",
+                "prenom":"Jonathan",
+                "login":"loginDeJo",
+                "adresses":[{"id":3,"numero":6,"rue":"rue athenas","ville":"Nantes"}]
+              },
+              "commandeProduits":[
+                {
+                  "id":4,
+                  "quantite":2,
+                  "produit":{"id":2,"libelle":"Moto","caracteristique":"Elle a 2 roues et un guidon","categorie":"Vehicule","image":"http://lorempixel.com/200/200/transport","prix":2999.99}
+                },
+                {
+                  "id":3,
+                  "quantite":6,
+                  "produit":{"id":1,"libelle":"Truc High-Tech","caracteristique":"Il sert ? rien mais il est cool","categorie":"High-Tech","image":"http://lorempixel.com/200/200/technics","prix":100.0}
+                }
+              ],
+              "etat":"Termin"
+            },
+            {
+              "id":4,
+              "client":{
+                "id":3,
+                "actif":true,
+                "nom":"LEBON",
+                "prenom":"Jonathan",
+                "login":"loginDeJo",
+                "adresses":[{"id":3,"numero":6,"rue":"rue athenas","ville":"Nantes"}]
+              },
+              "commandeProduits":[
+                {
+                  "id":1,
+                  "quantite":2,
+                  "produit":{"id":2,"libelle":"Moto","caracteristique":"Elle a 2 roues et un guidon","categorie":"Vehicule","image":"http://lorempixel.com/200/200/transport","prix":50}
+                },
+                {
+                  "id":5,
+                  "quantite":6,
+                  "produit":{"id":1,"libelle":"Truc High-Tech","caracteristique":"Il sert ? rien mais il est cool","categorie":"High-Tech","image":"http://lorempixel.com/200/200/technics","prix":100.0}
+                }
+              ],
+              "etat":"Termin"
+            }];
 
     beforeEach(function() {
 		module("ecDesktopApp.stats");
 	});
 
-it("Récupérer toutes les commandes", inject(function(ventesService, $httpBackend) {
+it("Récupérer toutes les commandes", inject(function(ventesService, $httpBackend,API_URL) {
 
-		$httpBackend.expectGET('http://localhost:3000/commande').respond(200, mockCommandes);
+		$httpBackend.expectGET(API_URL + '/commande').respond(200, mockCommandes);
 
 		ventesService.getCommandes().then(function (result){
-			expect(result.length).toBe(mockCommandes.length);
-			expect(result[0].user_id).toBe(mockCommandes[0].user_id);
-			expect(result[0].prix_total).toBe(mockCommandes[0].prix_total);
-			expect(result[0].panier[0].id).toBe(mockCommandes[0].panier[0].id);
-			expect(result[0].panier[0].prix).toBe(mockCommandes[0].panier[0].prix);
-			expect(result[0].panier[0].image).toBe(mockCommandes[0].panier[0].image);
-			expect(result[0].panier[0].quantite).toBe(mockCommandes[0].panier[0].quantite);
-			expect(result[0].panier[1].id).toBe(mockCommandes[0].panier[1].id);
-			expect(result[0].panier[1].prix).toBe(mockCommandes[0].panier[1].prix);
-			expect(result[0].panier[1].image).toBe(mockCommandes[0].panier[1].image);
-			expect(result[0].panier[1].quantite).toBe(mockCommandes[0].panier[1].quantite);
-			expect(result[0].type_paiement).toBe(mockCommandes[0].type_paiement);
-			expect(result[0].id).toBe(mockCommandes[0].id);
-			expect(result[0].date).toBe(mockCommandes[0].date);
+			expect(result.length).toEqual(mockCommandes.length);
+        expect(result[0].id).toEqual(mockCommandes[0].id);
+        expect(result[0].client).toEqual(mockCommandes[0].client);
+        expect(result[0].commandeProduits).toEqual(mockCommandes[0].commandeProduits);
+        expect(result[0].etat).toEqual(mockCommandes[0].etat);
+        expect(result[1].id).toEqual(mockCommandes[1].id);
+        expect(result[1].client).toEqual(mockCommandes[1].client);
+        expect(result[1].commandeProduits).toEqual(mockCommandes[1].commandeProduits);
+        expect(result[1].etat).toEqual(mockCommandes[1].etat);
+        expect(result[2].id).toEqual(mockCommandes[2].id);
+        expect(result[2].client).toEqual(mockCommandes[2].client);
+        expect(result[2].commandeProduits).toEqual(mockCommandes[2].commandeProduits);
+        expect(result[2].etat).toEqual(mockCommandes[2].etat);
 		});
 		$httpBackend.flush();
 	}));
