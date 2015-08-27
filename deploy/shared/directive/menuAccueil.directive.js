1390b7108c91ca7f35a61e9b47070998894d53b2
Jenkins <jenkins@ns377570.ip-5-196-89.eu>
2015-08-27 18:01:30 +0200

    auto deploy

---
+++ deploy/shared/directive/menuAccueil.directive.js
@@ -0,0 +1,7 @@
+angular.module('ecDesktopApp.shared')
+.directive('ecMenuAccueil', function(){
+	return {
+		restrict: 'E',
+		templateUrl: 'shared/directive/template/menuAccueil.tpl.html'
+	};
+});
