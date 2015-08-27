86a3bd06d1f6c310f1818b7936772b5ea2ab2563
Romain Farau <romain.farau@gmail.com>
2015-08-27 10:29:04 +0200

    #28 stable

--- shared/directive/menu.directive.js
+++ shared/directive/menu.directive.js
@@ -1,11 +1,10 @@
 angular.module('ecDesktopApp.shared')
 .directive('ecNavBarElements',function(DATA_MENU){
-
 });
 
 /*return {
-scope: {
-type: '=type'
-},
-templateUrl: 'template/my-navbar-element.html';
-};*/
+		scope: {
+			type: '=type'
+		},
+		templateUrl: 'template/my-navbar-element.html';
+	};*/
